685d1b950943439fe046e3838d6acfedffaf426a
amudajr <classicallow26@gmail.com>
2022-07-16 15:03:24 +0100

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 65.0;
+const kActiveCardColor = Color(0xFF1D1E33);
+const kInactiveCardColor = Color(0xFF111328);
+const kBottomContainer = Color(0xFFEB1555);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeText = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTittlePage = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const resultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyText = TextStyle(
+  fontSize: 22.0,
+);
